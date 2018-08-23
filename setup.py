2ac60b05ea31384ed4cf6fe71e9642396a1a7caf
Billie Thompson <billie.thompson@armakuni.co.uk>
2018-08-23 09:40:02 +0100

    Allow pypi install
    Currently you have to do a weird git+ command to install this archive
    with pipenv. This adds it to pypi so it's automatically installed.

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
     name='requestsdefaulter',
-    version='0.1.0',
-    packages=['requestsdefaulter'],
+    version='0.1.1',
     url='https://github.com/ONSdigital/requestsdefaulter',
     license='MIT',
-    author='ONS',
-    author_email='',
-    description='Small library to set default headers in requests'
+    author='RAS Development Team',
+    author_email='onsdigital@linux.co.uk',
+    description='Small library to set default headers in requests',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
